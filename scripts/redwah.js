dfb2cffe4e035333701a52fae016c70371bf1677
jesse keane <me@jessekeane.me>
2013-05-21 06:17:22 +0400

    adding some test data to the controller

---
+++ scripts/redwah.js
@@ -0,0 +1,13 @@
+function GridCtrl($scope) {
+  $scope.items = [
+    { "name": "House 1",
+      "qualities": [0, 0, 0, 0, 0]
+    },
+    { "name": "House 2",
+      "qualities": [0, 0, 0, 0, 0]
+    },
+    { "name": "House 3",
+      "qualities": [0, 0, 0, 0, 0]
+    }
+  ];
+}
