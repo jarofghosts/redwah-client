9f7f62e54526e3bef206a4858c340b7013cd0295
jesse keane <me@jessekeane.me>
2013-05-21 06:34:31 +0400

    i cant believe how easy this was...

--- scripts/redwah.js
+++ scripts/redwah.js
@@ -1,5 +1,5 @@
 function GridCtrl($scope) {
-  $scope.qualities = [ "Cold Fridge", "Outlets", "W/D", "Neighborhood" ];
+  $scope.qualities = [ "Good Kitchen",  "Cold Fridge", "Outlets", "W/D", "Neighborhood" ];
   $scope.items = [
     { "name": "House 1",
       "qualities": [0, 0, 0, 0, 0]
