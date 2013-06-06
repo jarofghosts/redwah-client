8a3d53a34f7a15ea7b399db2a41a9fdc36882bb1
jesse keane <me@jessekeane.me>
2013-06-06 19:57:06 +0400

    persistence kinda working

--- scripts/redwah.js
+++ scripts/redwah.js
@@ -4,12 +4,13 @@ function GridCtrl($scope, $http, $location) {
     id: null,
     rev: null
   };
+
   $scope.highestItem = -1;
   $scope.loaded = false;
   $scope.listName = '';
   $scope.qualities = [];
   $scope.items = [];
- 
+
   $scope.addQuality = function () {
     if (!$scope.qualityText.length) { return; }
     $scope.qualities.push($scope.qualityText);
@@ -72,13 +73,22 @@ function GridCtrl($scope, $http, $location) {
   };
   
   $scope.saveList = function () {
-
+    $http.put('http://projects.jessekeane.me/list',
+      { id: $scope.remote.id,
+        rev: $scope.remote.rev,
+        name: $scope.listName,
+        qualities: $scope.qualities,
+        items: $scope.items })
+          .success(function (data, status) {
+          })
+          .error(function (data, status) {
+          });
   };
 
   var generateList = function (newListName) {
     $http.post('http://projects.jessekeane.me/list', { name: newListName })
       .success(function (data, status) {
-        $location.hash("/" + data.id);
+        $location.path("/" + data.id);
         $scope.remote.id = data.id;
         $scope.remote.rev = data.rev;
         $scope.loaded = true;
@@ -89,9 +99,27 @@ function GridCtrl($scope, $http, $location) {
       });
   };
 
-  $scope.getList = function (listId) {
+  var loadList = function (listId) {
+    $http.get('http://projects.jessekeane.me/list?id=' + listId)
+      .success(function (data, status) {
+        $scope.remote.id = data._id;
+        $scope.remote.rev = data._rev;
+        $scope.items = data.items;
+        $scope.qualities = data.qualities;
+        $scope.listName = data.name;
+        $scope.loaded = true;
+      })
+      .error(function (data, status) {
+        alert('there was an error');
+      });
   };
 
   $scope.removeList = function () {
   };
+
+  if ($location.hash().length) {
+    loadList($location.hash().substring(1));
+  }
+ 
+
 }
